7321bde6e2a6653bf04a2e9aae78045ded8afdfe
Paulo Victor Moura <paulovsm@gmail.com>
2021-03-03 18:43:57 -0300

    Bypass authentication for testing

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,21 +1,21 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode, JwtHeader } from 'jsonwebtoken'
+//import { verify, decode, JwtHeader } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
-import * as jwksClient from 'jwks-rsa'
+//import { Jwt } from '../../auth/Jwt'
+//import { JwtPayload } from '../../auth/JwtPayload'
+//import * as jwksClient from 'jwks-rsa'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://uda-pvsm.us.auth0.com/.well-known/jwks.json'
-const client = jwksClient({
-  jwksUri: jwksUrl
-});
+// const jwksUrl = 'https://uda-pvsm.us.auth0.com/.well-known/jwks.json'
+// const client = jwksClient({
+//   jwksUri: jwksUrl
+// });
 
 export const handler = async (
   event: CustomAuthorizerEvent
